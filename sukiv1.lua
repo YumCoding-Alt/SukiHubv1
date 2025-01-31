e8a92b00235bc537114c0b6999e17df52cb8659a
YumCoding-Alt <bonguyen09052005@gmail.com>
2025-01-31 17:13:22 +0700

    Add files via upload

---
+++ sukiv1.lua
@@ -0,0 +1,6 @@
+Config = {
+    namescript = "SukiHubV1",--Tên Script
+    logoscript = "80923673245466",--ID Logo Script
+    tacgia = "Duy Tâm Mê Code"-- Tên Tác Giả
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tngaming123/vesion/refs/heads/main/scriptbffree.lua"))()
